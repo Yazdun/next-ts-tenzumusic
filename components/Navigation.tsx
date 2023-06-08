9c8bcf3032b0deddda80ec8c32e55a8f1d14832c
Yazdun <yazduntube@gmail.com>
2023-06-09 03:23:31 +0430

    feat: make navigatoin bar stick to the top

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,7 +3,7 @@ import { Container, ThemeToggle } from '@/components'
 
 export const Navigation: React.FC = () => {
   return (
-    <div className="border-b-2 dark:border-gray-700">
+    <div className="sticky top-0 bg-white border-b-2 dark:border-gray-700 dark:bg-black">
       <Container as="nav" className="flex items-center justify-between py-4">
         <Link href="/" className="font-semibold">
           Tenzumusic
