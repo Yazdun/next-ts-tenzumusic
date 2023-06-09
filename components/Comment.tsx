5165a60930763beab0fd1a0bf69b603e3900c1fe
Yazdun <yazduntube@gmail.com>
2023-06-09 04:32:19 +0430

    fix: change comment background

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -5,7 +5,7 @@ export const Comment = (props: { comment: IComment }) => {
   const { comment } = props
 
   return (
-    <li className="p-5 border-l-2 dark:border-gray-700 bg-slate-100 rounded-r-xl dark:bg-black">
+    <li className="p-5 bg-gray-100 border-l-2 dark:border-gray-700 rounded-r-xl dark:bg-black">
       <div className="flex items-center gap-1 lowercase">
         <FaUserCircle />
         {comment.email}
